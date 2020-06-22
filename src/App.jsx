5aafa484aa6fe53b7913071d4b890cb42719b349
Jan Škvařil <kondr4@seznam.cz>
2020-06-22 23:47:57 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.scss';
+
+
+import {
+  ContactForm
+  //Here write components you want to use
+} from '@janskvaril/gabro-framework'
+
+function App() {
+  return (
+    <div>
+      <ContactForm />
+      {/* Write you code here */}
+
+    </div>
+  );
+}
+
+export default App;
